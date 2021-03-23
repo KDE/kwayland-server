5ba30dcadb73baa6fe66dd0350ffe766d9f8da3d
Vlad Zahorodnii <vlad.zahorodnii@kde.org>
2021-03-23 19:08:56 +0000

    Fix a typo
    This probably needs to be currentPrimarySelection.
    
    
    (cherry picked from commit 8a399fc8971b81a997d05afacfd13c813baa0cc8)

--- src/server/seat_interface.cpp
+++ src/server/seat_interface.cpp
@@ -1151,7 +1151,7 @@ void SeatInterface::setFocusedKeyboardSurface(SurfaceInterface *surface)
 
         d->globalKeyboard.focus.primarySelections = primarySelectionDevices;
         for (auto primaryDataDevice : primarySelectionDevices) {
-            if (d->currentSelection) {
+            if (d->currentPrimarySelection) {
                 primaryDataDevice->sendSelection(d->currentPrimarySelection);
             } else {
                 primaryDataDevice->sendClearSelection();
